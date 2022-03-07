c2986b0d0e62cb41edf3e7be0dcd6b8b2afe763d
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2022-03-07 12:28:24 +0600

    ....

---
+++ arrays.php
@@ -0,0 +1,17 @@
+<?php
+
+// $array = ["John", "Mary", "Peter", "Maria"];
+
+// if(isset($array[3])){
+//     echo $array[3];
+// }else{
+//     echo "Array doesnot exist";
+// }
+
+$books = [
+    'comic' => "martin beach",
+    'thriler' => "sudden death",
+    'romantic' => "first moment"
+];
+
+echo ($books['thriler']);
